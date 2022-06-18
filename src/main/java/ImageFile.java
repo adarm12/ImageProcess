eebdada8269e5b9672ac802e4dd1d3e0596c42bb
Shanitkk <Shanitk2020@gmail.com>
2022-06-18 13:24:01 +0300

    .

--- src/main/java/ImageFile.java
+++ src/main/java/ImageFile.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+
 public class ImageFile {
 
     public static final File FILE = new File("C:\\Users\\shani\\Desktop\\לימודים שנה א\\מדמח\\קבצים תוכנית\\dora\\Dora.jpg");
